56fd72ea1f9c48fe9f667527fbe7fc997e36c6e3
Paulo Sérgio Pierdoná <paulosergiopierdona@gmail.com>
2024-06-14 20:01:55 -0300

    all class fix

--- object_oriented/src/Base.hpp
+++ object_oriented/src/Base.hpp
@@ -4,51 +4,49 @@
 #define CHAR_FREE ' '
 #define CHAR_WALL '|'
 
-#ifdef __linux__ 
-    #include<unistd.h> //* sleep
-    #define CLEAR "clear"
-    #define SLEEP_TIME 100000
-    #define CHAR_BORD '>'
-    #define CHAR_BLOCK '#'
-    clearScreen()
-    {
-        system(CLEAR);
-    };
+    #ifdef __linux__ 
+        #include<unistd.h> //* sleep
+        #define CLEAR "clear"
+        #define SLEEP_TIME 100000
+        #define CHAR_BORD '>'
+        #define CHAR_BLOCK '#'
+        clearScreen()
+        {
+            system(CLEAR);
+        };
 
-#elif _WIN32
+    #elif _WIN32
 
-    #include <windows.h> //* sleep
-    #include <conio.h> //* getchar & getch
-    #define CLEAR "cls"
-    #define SLEEP_TIME 100
-    #define CHAR_BORD 205
-    #define CHAR_BLOCK 219    
+        #include <windows.h> //* sleep
+        #include <conio.h> //* getchar & getch
+        #define CLEAR "cls"
+        #define SLEEP_TIME 100
+        #define CHAR_BORD 205
+        #define CHAR_BLOCK 219    
 
-    //how it works? i have no idia
-    void clearScreen()
-    {
-        HANDLE hOut;
-        COORD Position;
+        //how it works? i have no idia
+        void static clearScreen()
+        {
+            HANDLE hOut;
+            COORD Position;
 
-        hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+            hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
-        Position.X = 0;
-        Position.Y = 0;
-        SetConsoleCursorPosition(hOut, Position);
-    }
+            Position.X = 0;
+            Position.Y = 0;
+            SetConsoleCursorPosition(hOut, Position);
+        }
 
-    void usleep(int time)
-    {
-        Sleep(time);
-    };
+        void static usleep(int time)
+        {
+            Sleep(time);
+        };
 
-    int getKeyChar(void)
-    {
-        return getch();
-    }
-#else
-
-#endif
+        int static getKeyChar(void)
+        {
+            return getch();
+        }
+    #endif
 
 
 
